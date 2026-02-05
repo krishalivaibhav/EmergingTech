f49513524de6519317d038d59d8d5d5413b6b3cb
krishalivaibhav <158697719+krishalivaibhav@users.noreply.github.com>
2026-02-05 10:00:00 +0530

    API route validation

---
+++ app/routes/web.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def index(request: Request) -> HTMLResponse:
+    return request.app.state.templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "year": datetime.now().year,
+        },
+    )
+
